c77b5624e12b35c6a0e93ae563c9585df61b60f9
Leisa Trisler <leisatrisler@yahoo.com>
2023-05-08 05:31:51 -0700

    changes watched you tube videos for guidence

---
+++ cart_no_ui.py
@@ -0,0 +1,133 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+        self.prices = []
+        self.total = 0.0
+        self.items = [
+            {
+                "item": {
+                    "id": 2,
+                    "name": "Confident Carrot",
+                    "image": "./confident_carrot.png",
+                    "price": 0.99,
+                },
+                "item": {
+                    "id": 3,
+                    "name": "Romantic Radishes",
+                    "image": "./romantic_radishes.png",
+                    "price": 1.50,
+                },
+                "item": {
+                    "id": 4,
+                    "name": "Panic Pear",
+                    "image": "./panic-pear.png",
+                    "price": 0.75,
+                },
+                "item": {
+                    "id": 5,
+                    "name": "Obnoxious Onion",
+                    "image": "./obnoxious_onion.png",
+                    "price": 0.50,
+                },
+                "item": {
+                    "id": 6,
+                    "name": "Melancholy Mushroom",
+                    "image": "./melancholy_mushroom.png",
+                    "price": 1.25,
+                },
+                "item": {
+                    "id": 7,
+                    "name": "Affection Asparagus",
+                    "image": "./affection_asparagus.png",
+                    "price": 4.25,
+                },
+                "item": {
+                    "id": 8,
+                    "name": "Patient Peas",
+                    "image": "./patient_peas.png",
+                    "price": 2.25,
+                },
+                "item": {
+                    "id": 9,
+                    "name": "Bubbly Banana",
+                    "image": "./bubbly_banana.png",
+                    "price": 0.75,
+                },
+                "item": {
+                    "id": 10,
+                    "name": "Cheeky Cherries",
+                    "image": "./cheeky_cherries.png",
+                    "price": 3.75,
+                },
+                "item": {
+                    "id": 11,
+                    "name": "Rebellious Raspberries",
+                    "image": "./rebellious_raspberries.png",
+                    "price": 2.75,
+                },
+                "item": {
+                    "id": 12,
+                    "name": "Amused Apple",
+                    "image": "./amused_apple.png",
+                    "price": 1.75,
+                },
+            }
+        ]
+
+    def add_item(self, item_name, item_price):
+        self.items.append(item_name)
+        self.prices.append(item_price)
+        self.total += item_price
+
+    def remove_item(self, item_name):
+        if item_name in self.items:
+            index = self.items.index(item_name)
+            item_price = self.prices[index]
+            self.items.remove(item_name)
+            self.prices.remove(item_price)
+            self.total -= item_price
+
+    def get_total(self):
+        return self.total
+
+    def list_items(self):
+        for item in self.items:
+            my_item = item["item"]
+            print(my_item)
+
+
+def main():
+    my_cart = ShoppingCart()
+
+    while True:
+        action = input(
+            "Welcome to Veggie Tales, what would you like to do?:  (add/remove/checkout/listexit): "
+        )
+
+        if action == "add":
+            item_name = input("Please enter the item name: ")
+            item_price = float(input("Please enter the item price: "))
+            my_cart.add_item(item_name, item_price)
+            print("Item(s) added to cart.")
+
+        elif action == "remove":
+            item_name = input("Enter item name: ")
+            my_cart.remove_item(item_name)
+            print("Item removed from cart.")
+
+        elif action == "checkout":
+            total = my_cart.get_total()
+            print(f"Total price: ${total:.2f}")
+            break
+        elif action == "list":
+            my_cart.list_items()
+
+        elif action == "exit":
+            break
+
+        else:
+            print("Invalid action. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
--- shoppingcart.py
+++ shoppingcart.py
@@ -1,145 +1,136 @@
-#!/usr/bin/python
-
 import tkinter as tk
-from PIL import ImageTk, Image
-import tkinter as tk
+from PIL import Image, ImageTk
 
 
-# 1. Item Description, Image, File, Item Price
-# 2. Confident Carrot, confident carrot, .png, $0.99
-# 3. Romantic Radishes, romantic radishes, .png, $1.50
-# 4. Panic Pear, panic pear, .png, $0.75
-# 5. Obnoxious Onion, obnoxious onion, .png, $0.50
-# 6. Melancholy Mushroom, melancholy mushroom, .png, $1.25
-# 7. Affection Asparagus, affection asparagus, .png, $4.25
-# 8. Patient Peas, patient peas, .png, $2.25
-# 9. Bubbly Banana, bubbly banana, .png, $0.75
-# 10. Cheeky Cherries, cheeky cherries, .png, $3.75
-# 11. Rebellious Raspberries, rebellious raspberries,.png, $2.75
-# 12. Amused Apple, amused apple, .png, $1.75
+class ShoppingCart(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("vg Cart")
+        self.items = []
+        self.total = 0.0
 
-items = [
-    {
-        "item": {
-            "id": 2,
-            "name": "Confident Carrot",
-            "image": "./confident_carrot.png",
-            "price": 0.99,
-        },
-        "item": {
-            "id": 3,
-            "name": "Romantic Radishes",
-            "image": "./romantic_radishes.png",
-            "price": 1.50,
-        },
-        "item": {
-            "id": 4,
-            "name": "Panic Pear",
-            "image": "./panic-pear.png",
-            "price": 0.75,
-        },
-        "item": {
-            "id": 5,
-            "name": "Obnoxious Onion",
-            "image": "./obnoxious_onion.png",
-            "price": 0.50,
-        },
-        "item": {
-            "id": 6,
-            "name": "Melancholy Mushroom",
-            "image": "./melancholy_mushroom.png",
-            "price": 1.25,
-        },
-        "item": {
-            "id": 7,
-            "name": "Affection Asparagus",
-            "image": "./affection_asparagus.png",
-            "price": 4.25,
-        },
-        "item": {
-            "id": 8,
-            "name": "Patient Peas",
-            "image": "./patient_peas.png",
-            "price": 2.25,
-        },
-        "item": {
-            "id": 9,
-            "name": "Bubbly Banana",
-            "image": "./bubbly_banana.png",
-            "price": 0.75,
-        },
-        "item": {
-            "id": 10,
-            "name": "Cheeky Cherries",
-            "image": "./cheeky_cherries.png",
-            "price": 3.75,
-        },
-        "item": {
-            "id": 11,
-            "name": "Rebellious Raspberries",
-            "image": "./rebellious_raspberries.png",
-            "price": 2.75,
-        },
-        "item": {
-            "id": 12,
-            "name": "Amused Apple",
-            "image": "./amused_apple.png",
-            "price": 1.75,
-        },
-    }
-]
+        self.item_listbox = tk.Text(self, width=50, height=10)
+        self.item_listbox.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.cart_listbox = tk.Listbox(self, width=50)
+        self.cart_listbox.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.total_label = tk.Label(self, text="Total: $0.00")
+        self.total_label.pack()
+
+        self.images = []
+        for item in items:
+            my_items = item["item"]
+            print(my_items)
+            image = Image.open(my_items["image"])
+            image = image.resize((100, 100))
+            photo = ImageTk.PhotoImage(image)
+            self.images.append(photo)
+            self.item_listbox.image_create(tk.END, image=photo)
+            self.item_listbox.insert(tk.END, "\n" + my_items["name"])
+
+        self.add_button = tk.Button(self, text="Add to Cart", command=self.add_to_cart)
+        self.add_button.pack(pady=10)
+
+        self.remove_button = tk.Button(
+            self, text="Remove from Cart", command=self.remove_from_cart
+        )
+        self.remove_button.pack(pady=10)
+
+    def add_to_cart(self):
+        selected_index = self.item_listbox.index(tk.INSERT)
+        if selected_index:
+            selected_item = items[selected_index - 1]
+            self.cart_listbox.insert(tk.END, selected_item["name"])
+            self.total += selected_item["price"]
+            self.update_total_label()
+
+    def remove_from_cart(self):
+        selected_index = self.cart_listbox.curselection()
+        if selected_index:
+            removed_item = self.cart_listbox.get(selected_index)
+            self.cart_listbox.delete(selected_index)
+            selected_item = next(
+                (item for item in items if item["name"] == removed_item), None
+            )
+            if selected_item:
+                self.total -= selected_item["price"]
+                self.update_total_label()
+
+    def update_total_label(self):
+        self.total_label.config(text="Total: ${:.2f}".format(self.total))
 
 
-def add_to_cart():
-    item = entry.get()
-    if item:
-        cart.insert(tk.END, item)
-        entry.delete(0, tk.END)
-
-
-def remove_from_cart():
-    selected_indices = cart.curselection()
-    for index in reversed(selected_indices):
-        cart.delete(index)
-
-
-root = tk.Tk()
-root.title("Veggie Tales")
-
-
-entry = tk.Entry(root, width=30)
-entry.pack(pady=10)
-
-
-add_button = tk.Button(root, text="Add to Cart", command=add_to_cart)
-add_button.pack()
-
-
-cart = tk.Listbox(root, width=50)
-cart.pack(pady=10)
-
-
-remove_button = tk.Button(root, text="Remove from Cart", command=remove_from_cart)
-remove_button.pack()
-
-# Load and display images
-#   "item": {
-#         "id": 2,
-#         "name": "Confident Carrot",
-#         "image": "./confident_carrot.png",
-#         "price": 0.99,
-#     },
-images = []
-for item in items:
-    print(item)
-    details = item["item"]
-    image = Image.open(details["image"])
-    image = image.resize((250, 250))
-    photo = ImageTk.PhotoImage(image)
-    images.append(photo)
-
-    label = tk.Label(root, image=photo)
-    label.image = photo
-    label.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    items = [
+        {
+            "item": {
+                "id": 2,
+                "name": "Confident Carrot",
+                "image": "./confident_carrot.png",
+                "price": 0.99,
+            },
+            "item": {
+                "id": 3,
+                "name": "Romantic Radishes",
+                "image": "./romantic_radishes.png",
+                "price": 1.50,
+            },
+            "item": {
+                "id": 4,
+                "name": "Panic Pear",
+                "image": "./panic-pear.png",
+                "price": 0.75,
+            },
+            "item": {
+                "id": 5,
+                "name": "Obnoxious Onion",
+                "image": "./obnoxious_onion.png",
+                "price": 0.50,
+            },
+            "item": {
+                "id": 6,
+                "name": "Melancholy Mushroom",
+                "image": "./melancholy_mushroom.png",
+                "price": 1.25,
+            },
+            "item": {
+                "id": 7,
+                "name": "Affection Asparagus",
+                "image": "./affection_asparagus.png",
+                "price": 4.25,
+            },
+            "item": {
+                "id": 8,
+                "name": "Patient Peas",
+                "image": "./patient_peas.png",
+                "price": 2.25,
+            },
+            "item": {
+                "id": 9,
+                "name": "Bubbly Banana",
+                "image": "./bubbly_banana.png",
+                "price": 0.75,
+            },
+            "item": {
+                "id": 10,
+                "name": "Cheeky Cherries",
+                "image": "./cheeky_cherries.png",
+                "price": 3.75,
+            },
+            "item": {
+                "id": 11,
+                "name": "Rebellious Raspberries",
+                "image": "./rebellious_raspberries.png",
+                "price": 2.75,
+            },
+            "item": {
+                "id": 12,
+                "name": "Amused Apple",
+                "image": "./amused_apple.png",
+                "price": 1.75,
+            },
+        }
+    ]
+app = ShoppingCart()
+app.mainloop()
